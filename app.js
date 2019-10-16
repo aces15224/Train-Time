8ef0080bf75cb13238d6b0fdb2fd7f160ff57a8a
Anthony15224 <Anthony15224@hotmail.com>
2019-10-15 21:02:49 -0500

    meh

---
+++ app.js
@@ -0,0 +1,13 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAW28aiCOrJMk_W8gRqe4VOLVNrbXR_CYY",
+    authDomain: "train-schedule-6183d.firebaseapp.com",
+    databaseURL: "https://train-schedule-6183d.firebaseio.com",
+    projectId: "train-schedule-6183d",
+    storageBucket: "train-schedule-6183d.appspot.com",
+    messagingSenderId: "277963663150",
+    appId: "1:277963663150:web:da95837f8e6969d976bcbf",
+    measurementId: "G-RPNP2WZ775"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  var database = firebase.database();
